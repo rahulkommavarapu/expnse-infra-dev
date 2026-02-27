7f47f4dc5d0c54a401b455226e4dbb3979614c91
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-02-27 18:01:07 +0530

    Test

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "83s-remote-state-devlop"
+    bucket         = "83s-remote-state-devlop" 
     key            = "expense-dev-bastion"
     region         = "us-east-1"
     dynamodb_table = "83s-remote-state-devlop"

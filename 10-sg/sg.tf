8837bf04238548f5de52c264538f619d3642bd35
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-02-27 18:01:54 +0530

    Test

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -45,3 +45,15 @@ module "bastion_sg" {
   sg_tags        = var.sg_tags
   
 }
+# Create Security Group app-alb
+module "app_alb_sg" {
+  source         = "git::https://github.com/rahulkommavarapu/terraform-aws-securitygroup.git?ref=main"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_name        = "app-alb"
+  sg_description = "Created for backend ALB for instances expense dev"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  common_tags    = var.common_tags
+  sg_tags        = var.sg_tags
+  
+}

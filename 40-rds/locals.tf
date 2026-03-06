0912820581afcb5f0684aaf00515c761a08f2539
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-03-06 21:39:51 +0530

    terraform

---
+++ 40-rds/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  resource_name = "${var.project_name}-${var.environment}"
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
+}
+
+# db_subnet_group_name = aws_ssm_parameter.database_subnet_group_name.value

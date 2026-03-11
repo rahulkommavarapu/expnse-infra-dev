f84d5eccfc19c69f5d0d2728529c1650c62d6a88
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-03-11 14:19:14 +0530

    test

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -6,4 +6,4 @@
 #  ansible-playbook -i inventory mysql.yaml
 
 # pull
-ansible-pull -i localhost -U https://github.com/rahulkommavarapu225/Expense-Ansible-Roles-tf.git main.yaml -e component=backend -e environment=$1
+ansible-pull -i localhost, -c local -U https://github.com/rahulkommavarapu/Expense-Ansible-Roles-tf.git main.yaml -e component=backend -e environment=$1

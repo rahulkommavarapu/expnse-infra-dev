f3a38ca6779006ae0cbbd92265b062c9ca463e07
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-07-24 17:31:36 +0530

    Test

--- 80-web-alb/provider.tf
+++ 80-web-alb/provider.tf
@@ -6,10 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "83s-remote-state-devlop" 
+    bucket         = "83s-remote-state-env" 
     key            = "expense-dev-web-alb"
     region         = "us-east-1"
-    dynamodb_table = "83s-remote-state-devlop"
+    # dynamodb_table = "83s-remote-state-devlop"
+    use_lockfile = true
   }
 
 

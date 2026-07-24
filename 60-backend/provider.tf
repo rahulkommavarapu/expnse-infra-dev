e9e39a69275a6c5d8c78e06ad930867beefee492
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-07-24 17:42:48 +0530

    Test

--- 60-backend/provider.tf
+++ 60-backend/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "83s-remote-state-devlop" 
+    bucket         = "83s-remote-state-env" 
     key            = "expense-dev-backend"
     region         = "us-east-1"
     # dynamodb_table = "83s-remote-state-devlop"
